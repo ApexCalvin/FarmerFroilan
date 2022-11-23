2bc07d714654e5bbdd5f9f3fa2883547df76959a
Tarin <tarin@Zipcoders-MacBook-Pro-5.local>
2022-11-23 17:36:16 -0500

    Completed Animal name tests

--- src/main/java/Animal/Animal.java
+++ src/main/java/Animal/Animal.java
@@ -1,4 +1,34 @@
 package Animal;
 
-public class Animal {
+import Interfaces.Eater;
+import Interfaces.NoiseMaker;
+
+public class Animal implements Eater, NoiseMaker {
+
+    public String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    public Animal() {
+        name = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void eat() {
+
+    }
 }
