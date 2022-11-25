7c7225a82c3650ba6f09049b6287d301fe0282c4
Carolina Diazgranados <linacello@gmail.com>
2022-11-24 20:17:14 -0500

    added tests to shelter and played around with how it is a generic.

--- src/test/java/com/zipcodewilmington/froilansfarm/Shelter_Tests/Shelter_Tests.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Shelter_Tests/Shelter_Tests.java
@@ -1,4 +1,49 @@
 package com.zipcodewilmington.froilansfarm.Shelter_Tests;
 
+import Animal.Chicken;
+import Animal.Horse;
+import Shelter.Shelter;
+import org.junit.Assert;
+import org.junit.Test;
+import Shelter.FarmHouse;
+
 public class Shelter_Tests {
+    @Test
+    public void shelterTest_Size1(){
+        Shelter shelter = new Shelter();
+        Horse horse = new Horse();
+        shelter.add(horse);
+        int expected = 1;
+        int actual = shelter.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shelterTest_Size2(){
+        Shelter shelter = new Shelter();
+        Horse horse = new Horse();
+        Chicken chicken = new Chicken();
+        shelter.add(horse);
+        shelter.add(chicken);
+        int expected = 2;
+        int actual = shelter.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shelterTest_Size3(){
+        Shelter shelter = new Shelter();
+        Horse horse = new Horse();
+        Chicken chicken = new Chicken();
+        FarmHouse farm = new FarmHouse();
+        shelter.add(horse);
+        shelter.add(chicken);
+        shelter.add(farm);
+        int expected = 3;
+        int actual = shelter.size();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
