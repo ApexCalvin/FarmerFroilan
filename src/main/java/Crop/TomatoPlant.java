3e9435e079607ee82a266d7c11d623d8dacb5d06
Tarin <tarin@Zipcoders-MacBook-Pro-5.local>
2022-11-26 16:10:11 -0500

    harvested one corn

--- src/main/java/Crop/TomatoPlant.java
+++ src/main/java/Crop/TomatoPlant.java
@@ -1,7 +1,11 @@
 package Crop;
 
 import Food.Tomato;
+import Interfaces.Edible;
 
 public class TomatoPlant extends Crop<Tomato> {
-
+    @Override
+    public Edible yield() {
+        return new Tomato();
+    }
 }
