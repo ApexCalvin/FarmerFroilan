cf072b016ce45893c6ee7ce71a5c77b9512a847f
Tarin <tarin@Zipcoders-MacBook-Pro-5.local>
2022-11-27 21:49:51 -0500

    added nullary constructors and tests for chicken and horse class

--- src/test/java/com/zipcodewilmington/froilansfarm/Animal_Tests/Horse_Tests.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Animal_Tests/Horse_Tests.java
@@ -1,5 +1,6 @@
 package com.zipcodewilmington.froilansfarm.Animal_Tests;
 
+import Animal.Animal;
 import Animal.Chicken;
 import Animal.Horse;
 import org.junit.Test;
@@ -7,6 +8,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Horse_Tests {
+
+    @Test
+    public void getHorseNameTest() {
+        Horse horse = new Horse();
+        String expected = "brownie";
+        horse.setName(expected);
+        String actual = horse.getName();
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void getHorseNameTest2() {
+        Horse horse = new Horse("apple");
+        String actual = horse.getName();
+        String expected = "apple";
+        assertEquals(actual, expected);
+    }
 
     @Test
     public void riddenTest() {
